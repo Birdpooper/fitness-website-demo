9708eb9199007835c7470660908ed91f2106c1bb
Birdpooper <81507220+Birdpooper@users.noreply.github.com>
2024-01-27 17:35:28 +0545

    bmr calculator

--- javascript/fat.js
+++ javascript/fat.js
@@ -1,37 +1,74 @@
-// Basic BMR calculations
-function basalMetabolicRate() {
-  $("button").click(function () {
-    if (!$("input[type=radio]").is(":checked")) {
-      $(".noDisplayRadio").toggleClass("noDisplayRadio");
-    }
-    if ($("#height").val() == "") {
-      $(".noDisplayHeight").toggleClass("noDisplayHeight");
-    }
-    if ($("#weight").val() == "") {
-      $(".noDisplayWeight").toggleClass("noDisplayWeight");
-    }
-    if ($("#age").val() == "") {
-      $(".noDisplayAge").toggleClass("noDisplayAge");
-    } else {
-      var height = $("#height").val();
-      var weight = $("#weight").val();
-      var age = $("#age").val();
-      var gender = $("input[name=gender]:checked").val();
-      if (gender == "male") {
-        var bmr = Math.round(10 * weight + 6.25 * height - 5 * age + 5);
-        $(".bmr").text(bmr);
-      }
-      if (gender == "female") {
-        var bmr = Math.round(10 * weight + 6.25 * height - 5 * age - 161);
-        $(".bmr").text(bmr);
-      }
-      $(".bmr").css("display", "inline");
-      $(".inline").css("display", "inline");
-      $(".bmr1").toggleClass("bmr1");
-    }
-  });
-}
+const calories = document.querySelector(".bmr-calculator .result .calories");
+const calculateBtn = document.querySelector(
+  ".bmr-calculator .result .calculate-btn"
+);
+const age = document.querySelector(".bmr-calculator form #age");
+const height = document.querySelector(".bmr-calculator form #height");
+const weight = document.querySelector(".bmr-calculator form #weight");
+const errorMessage = document.querySelector(
+  ".bmr-calculator .result .error-message"
+);
 
-$(document).ready(function () {
-  basalMetabolicRate();
+// BMR = 10*weight + 6.25*height - 5*age + 5
+// BMR = 10*weight + 6.25*height - 5*age - 161;
+
+const calculateBMR = (weight, height, age, gender) => {
+  if (gender == "male") {
+    return 10 * weight + 6.25 * height - 5 * age + 5;
+  }
+
+  return 10 * weight + 6.25 * height - 5 * age - 161;
+};
+
+calculateBtn.addEventListener("click", () => {
+  if (
+    age.classList.contains("invalid") ||
+    height.classList.contains("invalid") ||
+    weight.classList.contains("invalid")
+  ) {
+    errorMessage.classList.add("active");
+    return;
+  }
+
+  errorMessage.classList.remove("active");
+
+  let genderValue = document.querySelector(
+    ".bmr-calculator form input[name='gender']:checked"
+  ).value;
+
+  let BMR = calculateBMR(weight.value, height.value, age.value, genderValue);
+
+  calories.innerHTML = BMR.toLocaleString("en-US");
+});
+
+// Input Validation
+
+age.addEventListener("input", (e) => {
+  let ageValue = e.target.value;
+
+  if (!ageValue || isNaN(ageValue) || ageValue < 10 || ageValue > 100) {
+    age.classList.add("invalid");
+  } else {
+    age.classList.remove("invalid");
+  }
+});
+
+height.addEventListener("input", (e) => {
+  let heightValue = e.target.value;
+
+  if (!heightValue || isNaN(heightValue) || heightValue < 0) {
+    height.classList.add("invalid");
+  } else {
+    height.classList.remove("invalid");
+  }
+});
+
+weight.addEventListener("input", (e) => {
+  let weightValue = e.target.value;
+
+  if (!weightValue || isNaN(weightValue) || weightValue < 0) {
+    weight.classList.add("invalid");
+  } else {
+    weight.classList.remove("invalid");
+  }
 });
